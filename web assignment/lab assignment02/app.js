de45a51eede44803f7999ff47a580a641ab166ff
Arisha- Inayat <inayataresha@gmail.com>
2026-04-05 19:26:14 +0500

    responsive

---
+++ web assignment/lab assignment02/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Set EJS as view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home route
+app.get('/', (req, res) => {
+    res.render('index'); // view/index.ejs ko render karega
+});
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
