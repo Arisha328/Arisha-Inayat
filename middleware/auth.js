83d0bf55cce8b9a2d363ef88a4e371e211bb42fe
Arisha- Inayat <inayataresha@gmail.com>
2026-05-13 13:56:31 +0500

    Clean repository

--- middleware/auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-    // Session check logic
-    if (req.session && req.session.isAdmin) {
-        return next();
-    }
-    // Agar login nahi hai to login page par redirect karein
-    res.redirect('/login'); 
-};
